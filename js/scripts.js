148d2c0f5ecebfc5733f0e2dbd53ec42d3007acb
YediRisdianto <yedi.risdianto1@gmail.com>
2024-10-17 00:29:06 +0700

    add : project page and contact form

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,38 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+    (function () {
+        emailjs.init("MGKTrkT5Yf2hcqF6T"); // Ganti YOUR_USER_ID dengan user ID dari EmailJS
+    })();
+
+    document.getElementById('contactForm').addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        // Validasi input form
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const phone = document.getElementById('phone').value;
+        const message = document.getElementById('message').value;
+
+        if (!name || !email || !phone || !message) {
+            document.getElementById('submitErrorMessage').classList.remove('d-none');
+            return;
+        }
+
+        // Kirim email melalui EmailJS
+        emailjs.send("service_ax2a4la", "template_n1l8all", {
+            name: name,
+            email: email,
+            phone: phone,
+            message: message,
+        })
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+            document.getElementById('submitSuccessMessage').classList.remove('d-none');
+            document.getElementById('submitErrorMessage').classList.add('d-none');
+            document.getElementById('contactForm').reset();
+        }, function (error) {
+            console.log('FAILED...', error);
+            document.getElementById('submitSuccessMessage').classList.add('d-none');
+            document.getElementById('submitErrorMessage').classList.remove('d-none');
+        });
+    });
+
